dd2aec22076b0b230ae4b5759c82dab3c0f86cad
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:26 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,17 @@
-output "site_recovery_fabrics" {
-  description = "All site_recovery_fabric resources"
-  value       = azurerm_site_recovery_fabric.site_recovery_fabrics
-}
 output "site_recovery_fabrics_location" {
-  description = "List of location values across all site_recovery_fabrics"
-  value       = [for k, v in azurerm_site_recovery_fabric.site_recovery_fabrics : v.location]
+  description = "Map of location values across all site_recovery_fabrics, keyed the same as var.site_recovery_fabrics"
+  value       = { for k, v in azurerm_site_recovery_fabric.site_recovery_fabrics : k => v.location }
 }
 output "site_recovery_fabrics_name" {
-  description = "List of name values across all site_recovery_fabrics"
-  value       = [for k, v in azurerm_site_recovery_fabric.site_recovery_fabrics : v.name]
+  description = "Map of name values across all site_recovery_fabrics, keyed the same as var.site_recovery_fabrics"
+  value       = { for k, v in azurerm_site_recovery_fabric.site_recovery_fabrics : k => v.name }
 }
 output "site_recovery_fabrics_recovery_vault_name" {
-  description = "List of recovery_vault_name values across all site_recovery_fabrics"
-  value       = [for k, v in azurerm_site_recovery_fabric.site_recovery_fabrics : v.recovery_vault_name]
+  description = "Map of recovery_vault_name values across all site_recovery_fabrics, keyed the same as var.site_recovery_fabrics"
+  value       = { for k, v in azurerm_site_recovery_fabric.site_recovery_fabrics : k => v.recovery_vault_name }
 }
 output "site_recovery_fabrics_resource_group_name" {
-  description = "List of resource_group_name values across all site_recovery_fabrics"
-  value       = [for k, v in azurerm_site_recovery_fabric.site_recovery_fabrics : v.resource_group_name]
+  description = "Map of resource_group_name values across all site_recovery_fabrics, keyed the same as var.site_recovery_fabrics"
+  value       = { for k, v in azurerm_site_recovery_fabric.site_recovery_fabrics : k => v.resource_group_name }
 }
 
